f3f3a5e1b0d8537629abc7a4bd38b084bd832d3d
Daniel Fletcher <dfletcher@sruc.ac.uk>
2025-09-24 14:03:44 +0100

    finishinf of manure storage

--- src/ExtractAgreCalcManureStorageData.py
+++ src/ExtractAgreCalcManureStorageData.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
+import matplotlib as mpl
 import geopandas as gpd
 import warnings
 from pandas.errors import SettingWithCopyWarning
@@ -16,16 +17,18 @@ warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 import requests
 
 from ExtractJASManureHandlingData import map_df,nuts_df #for mapping
-from ExtractAgreCalcHousing import add_prop_item, calc_weighted_mean,itl_scot
+from ExtractAgreCalcHousing import add_prop_item, calc_weighted_mean,itl_scot, NUTS2,LADS,map_df,nuts_df
 
 
 import textwrap
 
-save_dir='../output/ManureHandlingSystem/AC/'
+save_dir='../output/ManureStorageType/AC/'
 agrecalc_path='D:\\AgreCalc\\BeefSAG_2025\\20250728-SAGBeef_LAD.csv'
 
-
-
+plotting = 'nuts'
+geoplotting={'nuts':{'shapes':NUTS2,'col':'NUTS2','df':nuts_df,'map_col':'ITL225CD'},
+                 'LAD':{'shapes':LADS,'col':'LAD_CODE','df':map_df,'map_col':'LAD22CD'}
+                        } #to format plotting below
 
 df_raw=pd.read_csv(agrecalc_path)
 df_raw=df_raw.drop(columns=['Housed (%)'])
@@ -83,13 +86,14 @@ for m in solid_cols:
     df_beef_clean[m+'_normed']=df_beef_clean[m]/df_beef_clean['solid_total']
 for m in liquid_cols:
     df_beef_clean[m+'_normed']=df_beef_clean[m]/df_beef_clean['liquid_total']
-
-
-#df_beef_clean.to_csv('test.csv')
     
+df_solid=df_beef_clean[df_beef_clean['solid_total']>0]
+df_liquid=df_beef_clean[df_beef_clean['liquid_total']>0]
+
+'''
 ##first do mean and std for solid
 #only include farms whicih include some solid
-df_solid=df_beef_clean[df_beef_clean['solid_total']>0]
+
 #add in the proportion of all beef animals thats is in each row. Doesn't distinguish between animal types.
 df_solid=add_prop_item(df_solid,beef_types)
 n_farms=df_solid['Report ID'].nunique() #count untious farm_years
@@ -102,11 +106,12 @@ for m in solid_cols:
     #calculate number of animals which have at least some of this this type
     out_solid[m]=d
 check_sum_solid=np.sum([v['mean'] for v in out_solid.values()])
-    
+out_solid_df=pd.DataFrame.from_dict(out_solid)
+out_solid_df.to_csv(save_dir+'solid_manure_AC.csv')    
  
 #Now do liquid    
 #only include farms whicih include some liquid
-df_liquid=df_beef_clean[df_beef_clean['liquid_total']>0]
+
 #add in the proportion of all beef animals thats is in each row. Doesn't distinguish between animal types.
 df_liquid=add_prop_item(df_liquid,beef_types)
 n_farms=df_liquid['Report ID'].nunique() #count untious farm_years
@@ -119,8 +124,154 @@ for m in liquid_cols:
     #calculate number of animals which have at least some of this this type
     out_liquid[m]=d
 check_sum_liquid=np.sum([v['mean'] for v in out_liquid.values()])
+out_liquid_df=pd.DataFrame.from_dict(out_liquid)
+out_liquid_df.to_csv(save_dir+'liquid_manure_AC.csv')
+'''
 
 
 
+###Spatial plots LIQUID
+df_beef_geo_liquid=df_liquid[~df_liquid[geoplotting[plotting]['col']].isna()] #drop invalid postcodes
+out_liquid_region={} #{LAD:{bt:{housed:housed...,n_cattle:}}}
+for counter,LAD in enumerate(geoplotting[plotting]['shapes']):
+    df_lad=df_beef_geo_liquid[df_beef_geo_liquid[geoplotting[plotting]['col']]==LAD]
+    df_lad=add_prop_item(df_lad,beef_types)
+    inner_dict={}
+    for m in liquid_cols:
+        mean,std=calc_weighted_mean(df_lad,m+'_normed','prop')
+        inner_dict[m]=mean*100
+    out_liquid_region[LAD]=inner_dict
+    
+#join this data onto map_df    
+gdf=geoplotting[plotting]['df'] #pick it out for ease of syntax
+fig,axs=plt.subplots(nrows=1,ncols=2,figsize=(10, 6))
+axs=axs.flatten()
+#I want to get a global vmax and min so extrac data first
+all_values=[]
+for m in liquid_cols:
+    all_values += [v[m] for v in out_liquid_region.values()]
+
+vmin = min(all_values)
+vmax = max(all_values)
+norm = Normalize(vmin=vmin, vmax=vmax)
+cmap = 'viridis'
+xmin, ymin, xmax, ymax = gdf.total_bounds #bounds of map
+
+for i,m in enumerate(liquid_cols):
+    all_values += [v[m] for v in out_liquid_region.values()]
+    mapper_dict={k:v[m] for k,v in out_liquid_region.items()} #to join
+    gdf[m+'_mean']=gdf[geoplotting[plotting]['map_col']].map(mapper_dict)
+    #plot
+    
+    gdf.plot(column=m + '_mean', cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
+    
+
+    #axs[i].set_aspect('auto')
+    axs[i].axis('off')
+    wrapped_title = textwrap.fill(m, width=40)
+    axs[i].set_title(wrapped_title,fontsize=11)
+
+axs[-1].axis('off')     #had not needed AXIS                
+# Shared colorbar
+#fig.subplots_adjust(right=0.8) #space for colorbar
+sm = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
+sm._A = []
+#cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+cbar = fig.colorbar(sm, ax=axs)
+cbar.set_label('% liquid manure stored as titled type', fontsize=16)
+
+#plt.tight_layout()
+#plt.subplots_adjust(wspace=0, right=0.9)  # wspace controls space between plots
+plt.savefig(save_dir+'liquid_manure_AC_{}.png'.format(geoplotting[plotting]['col']),dpi=300)
 
 
+#Spatial plots ###############SOLID
+df_beef_geo_solid=df_solid[~df_solid[geoplotting[plotting]['col']].isna()] #drop invalid postcodes
+out_solid_region={} #{LAD:{bt:{housed:housed...,n_cattle:}}}
+for counter,LAD in enumerate(geoplotting[plotting]['shapes']):
+    df_lad=df_beef_geo_solid[df_beef_geo_solid[geoplotting[plotting]['col']]==LAD]
+    df_lad=add_prop_item(df_lad,beef_types)
+    inner_dict={}
+    for m in solid_cols:
+        mean,std=calc_weighted_mean(df_lad,m+'_normed','prop')
+        inner_dict[m]=mean*100
+    out_solid_region[LAD]=inner_dict
+    
+#join this data onto map_df    
+gdf=geoplotting[plotting]['df'] #pick it out for ease of syntax
+fig,axs=plt.subplots(nrows=2,ncols=2,figsize=(7, 8))
+axs=axs.flatten()
+#I want to get a global vmax and min so extrac data first
+all_values=[]
+for m in solid_cols:
+    all_values += [v[m] for v in out_solid_region.values()]
+
+vmin = min(all_values)
+vmax = max(all_values)
+norm = Normalize(vmin=vmin, vmax=vmax)
+cmap = 'viridis'
+xmin, ymin, xmax, ymax = gdf.total_bounds #bounds of map
+
+for i,m in enumerate(solid_cols):
+    all_values += [v[m] for v in out_solid_region.values()]
+    mapper_dict={k:v[m] for k,v in out_solid_region.items()} #to join
+    gdf[m+'_mean']=gdf[geoplotting[plotting]['map_col']].map(mapper_dict)
+    #plot
+    
+    gdf.plot(column=m + '_mean', cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
+    
+
+    #axs[i].set_aspect('auto')
+    axs[i].axis('off')
+    wrapped_title = textwrap.fill(m, width=40)
+    axs[i].set_title(wrapped_title,fontsize=11)
+
+axs[-1].axis('off')     #had not needed AXIS                
+# Shared colorbar
+#fig.subplots_adjust(right=0.8) #space for colorbar
+sm = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
+sm._A = []
+#cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+cbar = fig.colorbar(sm, ax=axs)
+cbar.set_label('% solid manure stored as titled type', fontsize=16)
+
+#plt.tight_layout()
+#plt.subplots_adjust(wspace=0, right=0.9)  # wspace controls space between plots
+plt.savefig(save_dir+'solid_manure_AC_{}.png'.format(geoplotting[plotting]['col']),dpi=300)
+
+'''
+#In time plots
+liquid_plot={m:[] for m in liquid_cols}
+for y in years:
+    df_year=df_liquid[df_liquid["Year End"]==y]
+    df_year=add_prop_item(df_year,beef_types)
+    for m in liquid_cols:
+        mean,std=calc_weighted_mean(df_year,m+'_normed','prop')
+        liquid_plot[m].append(mean)
+
+
+for m in liquid_cols:
+    plt.plot(years,liquid_plot[m],label=m)
+plt.xlabel('Year')
+plt.ylabel('Percent of liquid manure managed as')    
+plt.legend()
+plt.savefig(save_dir+'liquid_in_time_AC.png', dpi=200)
+
+#In time plots SOLID
+solid_plot={m:[] for m in solid_cols}
+for y in years:
+    df_year=df_solid[df_solid["Year End"]==y]
+    df_year=add_prop_item(df_year,beef_types)
+    for m in solid_cols:
+        mean,std=calc_weighted_mean(df_year,m+'_normed','prop')
+        solid_plot[m].append(mean)
+plt.close('all')
+
+for m in solid_cols:
+    plt.plot(years,solid_plot[m],label=m)
+plt.xlabel('Year')
+plt.ylabel('Percent of solid manure managed as')    
+plt.legend()
+plt.savefig(save_dir+'solid_in_time_AC.png', dpi=200)
+plt.close('all')
+'''
--- src/ExtractJASManureStorageData.py
+++ src/ExtractJASManureStorageData.py
@@ -13,7 +13,7 @@ import warnings
 from pandas.errors import SettingWithCopyWarning
 warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
-from ExtractJASManureHandlingData import  beef_items, manure_storage_items, solid_manure_items, liquid_manure_items, df, map_df, converter_df, add_proprtion_of_items,calc_weighted_mean,LADS
+from ExtractJASManureHandlingData import  beef_items, manure_storage_items, solid_manure_items, liquid_manure_items, df, map_df,nuts_df,itl_scot,NUTS2, add_proprtion_of_items,calc_weighted_mean,LADS
 import textwrap
 from copy import deepcopy
 
@@ -31,7 +31,12 @@ def nan_divide(col1,col2): #for making sure A/B returns nan when B=0 in pandas
 if __name__=="__main__":
     beef_farm_cutoff=10# how many beef cows required to be called a beef farm
     save_dir='../output/ManureStorageType/'
+    #Switches for plotting if needed
+    plotting='LAD' #chose #nuts or LAD to decide what gets plot.
     
+    geoplotting={'nuts':{'shapes':NUTS2,'col':'ITL225CD','df':nuts_df,'map_col':'ITL225CD'},
+                 'LAD':{'shapes':LADS,'col':'LAD22CD','df':map_df,'map_col':'LAD22CD'}
+                        } #to format plotting below
     
     df_beef_cols=df[[b[0] for b in beef_items]] #only beef item cols
     df_beef=df[df_beef_cols.sum(axis=1)>beef_farm_cutoff] #only include farms which have more than x  beef cows in total.
@@ -65,7 +70,7 @@ if __name__=="__main__":
         df_beef_clean[m+'_normed']=df_beef_clean[m]/df_beef_clean['liquid_total']
     
     
-    
+    '''
     #First we analyse the whole country starting with solid.
     #We need to only include farms which hace some solid
     df_solid=df_beef_clean[df_beef_clean['solid_total']>0].copy()
@@ -98,39 +103,38 @@ if __name__=="__main__":
     
     df_out_liquid=pd.DataFrame.from_dict(out_liquid)
     df_out_liquid.to_csv(save_dir+'liquid_manure_means.csv')    
-    
-    
-    
-    
-    
     '''
-    ##Spatial analysi for solid
+    
+    
+    
+    
+    df_beef_geo=df_beef_clean[~df_beef_clean[geoplotting[plotting]['col']].isna()] #drop the rows with no lads code
+    
+    
+    
+    ##Spatial analysi for ###SOLID
     out_solid_region={} #{LAD:{manure_group:percenatge}}
-    counter=0
-    for LAD in LADS:
-        print("on {} out of {} for solid".format(counter,len(LADS)))
-        df_lad=df_beef_clean[df_beef_clean['LAD22CD']==LAD] #this particular LAD
+
+    for counter,LAD in enumerate(geoplotting[plotting]['shapes']):
+        df_lad=df_beef_geo[df_beef_geo[geoplotting[plotting]['col']]==LAD]
         inner_dict={}
+        df_lad=add_proprtion_of_items(df_lad, [b[0] for b in beef_items],prefix='beef')
         for m in solid_manure_items:
-            df_for_calc=df_lad[~df_lad[m+'_normed'].isna()] #remove nans in this manure type. 
-            #df_for_calc=deepcopy(df_beef_clean)
-            df_for_calc_prop=add_proprtion_of_items(df_for_calc, [b[0] for b in beef_items],prefix='beef')
-            #We need to make sure each solid 
-            if len(df_for_calc_prop)==0: #if no data here
+            df_for_calc=deepcopy(df_lad)
+             
+            if len(df_for_calc)==0: #if no data here
                 weighted_mean=np.nan
             else:
-                weighted_mean=df_for_calc_prop.apply(lambda row: row[m+'_normed']*row['beef_prop'],axis=1).sum() #this assumes all animals produce the same amount of managed manure.
-            #weighted_std=np.sqrt(df_for_calc_prop.apply(lambda row: (row[m+'_normed']-weighted_mean)**2*row['beef_prop'],axis=1).sum())
-            #n_farms=len(df_for_calc_prop)
-            #calcualte how many cows included in calculation
-            #n_cattle=df_for_calc_prop.apply(lambda row: row[[b[0] for b in beef_items]].sum(),axis=1).sum()
+                weighted_mean,weighted_std=calc_weighted_mean(df_for_calc,m+'_normed','beef_prop')
+            
+            n_farms=len(df_for_calc)
+            n_cattle=df_for_calc.apply(lambda row: row[[b[0] for b in beef_items]].sum(),axis=1).sum()
             inner_dict[m]=weighted_mean*100 #make it percent        
         out_solid_region[LAD]=inner_dict
-        counter+=1
         
     #join this data onto map_df    
-
-    fig,axs=plt.subplots(nrows=int(np.ceil(len(solid_manure_items)/2)),ncols=2, figsize=(1 * len(solid_manure_items), 9), constrained_layout=True)
+    gdf=geoplotting[plotting]['df'] #pick it out for ease of syntax
+    fig,axs=plt.subplots(nrows=int(np.ceil(len(solid_manure_items)/2)),ncols=2, figsize=(6, 9), constrained_layout=True)
     axs=axs.flatten()
     #I want to get a global vmax and min so extrac data first
     all_values=[]
@@ -141,21 +145,21 @@ if __name__=="__main__":
     vmax = max(all_values)
     norm = Normalize(vmin=vmin, vmax=vmax)
     cmap = 'viridis'
-    xmin, ymin, xmax, ymax = map_df.total_bounds #bounds of map
+    xmin, ymin, xmax, ymax = gdf.total_bounds #bounds of map
     
     for i,m in enumerate(solid_manure_items):
         all_values += [v[m] for v in out_solid_region.values()]
         mapper_dict={k:v[m] for k,v in out_solid_region.items()} #to join
-        map_df[m+'_mean']=map_df['LAD22CD'].map(mapper_dict)
+        gdf[m+'_mean']=gdf[geoplotting[plotting]['map_col']].map(mapper_dict)
         #plot
         
-        map_df.plot(column=map_df[m + '_mean'], cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
+        gdf.plot(column=m + '_mean', cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
         
     
         #axs[i].set_aspect('auto')
         axs[i].axis('off')
-        wrapped_title = textwrap.fill(manure_storage_items_dict[m], width=40)
-        axs[i].set_title(wrapped_title,fontsize=7)
+        wrapped_title = textwrap.fill(manure_storage_items_dict[m], width=30)
+        axs[i].set_title(wrapped_title,fontsize=10)
     
     axs[-1].axis('off')     #had not needed AXIS                
     # Shared colorbar
@@ -164,40 +168,39 @@ if __name__=="__main__":
     sm._A = []
     #cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     cbar = fig.colorbar(sm, ax=axs)
-    cbar.set_label('% liquid manure stored as titled type', fontsize=10)
+    cbar.set_label('% solid manure stored as titled type', fontsize=16)
     
     #plt.tight_layout()
     #plt.subplots_adjust(wspace=0, right=0.9)  # wspace controls space between plots
-    plt.savefig(save_dir+'solid_manure_regional.png',dpi=300)      
-    '''
-    '''
+    plt.savefig(save_dir+'solid_manure_{}.png'.format(geoplotting[plotting]['col']),dpi=300)      
+    
+    
+
+    
+    
     #Spatial analysis for liquid.
     out_liquid_region={} #{LAD:{manure_group:percenatge}}
-    counter=0
-    for LAD in LADS:
-        print("on {} out of {} for liquid".format(counter,len(LADS)))
-        df_lad=df_beef_clean[df_beef_clean['LAD22CD']==LAD] #this particular LAD
+
+    for counter,LAD in enumerate(geoplotting[plotting]['shapes']):
+        df_lad=df_beef_geo[df_beef_geo[geoplotting[plotting]['col']]==LAD]
         inner_dict={}
+        df_lad=add_proprtion_of_items(df_lad, [b[0] for b in beef_items],prefix='beef')
         for m in liquid_manure_items:
-            df_for_calc=df_lad[~df_lad[m+'_normed'].isna()] #remove nans in this manure type. 
-            #df_for_calc=deepcopy(df_beef_clean)
-            df_for_calc_prop=add_proprtion_of_items(df_for_calc, [b[0] for b in beef_items],prefix='beef')
-            #We need to make sure each liquid 
-            if len(df_for_calc_prop)==0: #if no data here
+            df_for_calc=deepcopy(df_lad)
+             
+            if len(df_for_calc)==0: #if no data here
                 weighted_mean=np.nan
             else:
-                weighted_mean=df_for_calc_prop.apply(lambda row: row[m+'_normed']*row['beef_prop'],axis=1).sum() #this assumes all animals produce the same amount of managed manure.
-            #weighted_std=np.sqrt(df_for_calc_prop.apply(lambda row: (row[m+'_normed']-weighted_mean)**2*row['beef_prop'],axis=1).sum())
-            #n_farms=len(df_for_calc_prop)
-            #calcualte how many cows included in calculation
-            #n_cattle=df_for_calc_prop.apply(lambda row: row[[b[0] for b in beef_items]].sum(),axis=1).sum()
+                weighted_mean,weighted_std=calc_weighted_mean(df_for_calc,m+'_normed','beef_prop')
+            
+            n_farms=len(df_for_calc)
+            n_cattle=df_for_calc.apply(lambda row: row[[b[0] for b in beef_items]].sum(),axis=1).sum()
             inner_dict[m]=weighted_mean*100 #make it percent        
         out_liquid_region[LAD]=inner_dict
-        counter+=1
         
     #join this data onto map_df    
-
-    fig,axs=plt.subplots(nrows=int(np.ceil(len(liquid_manure_items)/2)),ncols=2, figsize=(2 * len(liquid_manure_items), 9), constrained_layout=True)
+    gdf=geoplotting[plotting]['df'] #pick it out for ease of syntax
+    fig,axs=plt.subplots(nrows=int(np.ceil(len(liquid_manure_items)/2)),ncols=2, figsize=(10, 10), constrained_layout=True)
     axs=axs.flatten()
     #I want to get a global vmax and min so extrac data first
     all_values=[]
@@ -208,21 +211,21 @@ if __name__=="__main__":
     vmax = max(all_values)
     norm = Normalize(vmin=vmin, vmax=vmax)
     cmap = 'viridis'
-    xmin, ymin, xmax, ymax = map_df.total_bounds #bounds of map
+    xmin, ymin, xmax, ymax = gdf.total_bounds #bounds of map
     
     for i,m in enumerate(liquid_manure_items):
         all_values += [v[m] for v in out_liquid_region.values()]
         mapper_dict={k:v[m] for k,v in out_liquid_region.items()} #to join
-        map_df[m+'_mean']=map_df['LAD22CD'].map(mapper_dict)
+        gdf[m+'_mean']=gdf[geoplotting[plotting]['map_col']].map(mapper_dict)
         #plot
         
-        map_df.plot(column=map_df[m + '_mean'], cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
+        gdf.plot(column=m + '_mean', cmap=cmap, norm=norm,ax=axs[i], legend=False,missing_kwds={'color': 'lightgrey'})
         
     
         #axs[i].set_aspect('auto')
         axs[i].axis('off')
-        wrapped_title = textwrap.fill(manure_storage_items_dict[m], width=30)
-        axs[i].set_title(wrapped_title,fontsize=7)
+        wrapped_title = textwrap.fill(manure_storage_items_dict[m], width=35)
+        axs[i].set_title(wrapped_title,fontsize=10)
     
     axs[-1].axis('off')     #had not needed AXIS                
     # Shared colorbar
@@ -231,12 +234,16 @@ if __name__=="__main__":
     sm._A = []
     #cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     cbar = fig.colorbar(sm, ax=axs)
-    cbar.set_label('% liquid manure stored as titled type', fontsize=10)
+    cbar.set_label('% liquid manure stored as titled type', fontsize=16)
     
     #plt.tight_layout()
     #plt.subplots_adjust(wspace=0, right=0.9)  # wspace controls space between plots
-    plt.savefig(save_dir+'liquid_manure_regional.png',dpi=300)       
-    '''    
+    plt.savefig(save_dir+'liquid_manure_{}.png'.format(geoplotting[plotting]['col']),dpi=300)
+    
+    
+    
+   
+
         
         
   
